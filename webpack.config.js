c1bfb0f63e67514d614c537c48795a82500c16c1
mb4ndeira <mbandeira.cs@gmail.com>
2022-05-21 18:23:10 +0000

    chore: divide entry points to allow code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     }
 }
