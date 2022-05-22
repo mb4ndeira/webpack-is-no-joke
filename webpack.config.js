bfc6cbadf7f644ca3b6ed08922ae1e74a130b65f
mb4ndeira <mbandeira.cs@gmail.com>
2022-05-22 11:19:09 +0000

    chore: add babel loaders to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
             {
                 test:/\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { 
+                    loader: 'babel-loader', 
+                    options: { presets: ['@babel/preset-env'] }
+                }
             }
         ]
     },
